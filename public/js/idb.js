ac2b229d2911643b08739c732c8dda2cfb729706
Stephanie George <stephaniegeorge@Stephanies-Air.fios-router.home>
2021-08-22 13:12:53 -0400

    event handler

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,3 +2,28 @@
 let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+// upon a successful 
+request.onsuccess = function (event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+        // we haven't created this yet, but we will soon, so let's comment it out for now
+        // uploadTransaction();
+    }
+};
+
+request.onerror = function (event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
