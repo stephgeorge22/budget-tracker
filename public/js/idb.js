eabac8059c3ac73de7094d2acc7eea38a20d6de7
Stephanie George <stephaniegeorge@Stephanies-Air.fios-router.home>
2021-08-22 14:11:08 -0400

    sucessful transaction after internet connection is restored

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,8 +7,8 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function (event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+    // create an object store (table) called `new_action`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_action', { autoIncrement: true });
 };
 
 // upon a successful 
@@ -16,10 +16,10 @@ request.onsuccess = function (event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadAction() function to send all local db data to api
     if (navigator.onLine) {
         // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadTransaction();
+        uploadAction();
     }
 };
 
@@ -27,3 +27,62 @@ request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
 };
+
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_action'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const actionObjectStore = transaction.objectStore('new_action');
+
+    // add record to your store with add method
+    actionObjectStore.add(record);
+}
+
+function uploadAction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_action'], 'readwrite');
+
+    // access your object store
+    const actionObjectStore = transaction.objectStore('new_action');
+
+    // get all records from store and set to a variable
+    const getAll = actionObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_action'], 'readwrite');
+                    // access the new_action object store
+                    const actionObjectStore = transaction.objectStore('new_action');
+                    // clear all items in your store
+                    actionObjectStore.clear();
+
+                    alert('All saved actions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadAction);
