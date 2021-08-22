6a0ea667233edf9176862c2834e44d5bf68d1f79
Stephanie George <stephaniegeorge@Stephanies-Air.fios-router.home>
2021-08-22 17:07:59 -0400

    cached files update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,20 @@
 const FILES_TO_CACHE = [
-    "./js/index.js",
-    "./js/idb.js",
-    "./index.html",
-    "./css/styles.css", 
+    "/",
+    "/js/index.js",
+    "/js/idb.js",
+    "/index.html",
+    "/css/styles.css", 
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "./manifest.json"
+    "/manifest.json", 
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "icons/icon-72x72.png",
+    "icons/icon-96x96.png",
+    "icons/icon-128x128.png",
+    "icons/icon-144x144.png",
+    "icons/icon-152x152.png",
+    "icons/icon-192x192.png",
+    "icons/icon-384x384.png",
+    "icons/icon-512x512.png"
 ];
 
 const APP_PREFIX = 'Budget-';
